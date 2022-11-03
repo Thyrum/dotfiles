5992922ca4b5336864ae0055c68d6f3a07787742
Thyrum <adj00@protonmail.com>
2022-11-03 12:08:43 +0100

    [neovim-lua] autoinstall treesitter instead of all
    On some systems, installing all requires too much memory.

--- neovim-lua/.config/nvim/after/plugin/treesitter.lua
+++ neovim-lua/.config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,8 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = "all",
+	ensure_installed = { "lua" },
 	ignore_install = { "latex" },
 	sync_install = false,
+	auto_install = true,
 
 	highlight = {
 		enable = true,
