f249457fb9071a99f936b41c82acb80148150f88
Thyrum <adj00@protonmail.com>
2022-11-03 12:05:43 +0100

    [neovim-lua] Only run tex snippets on line_begin

--- neovim-lua/.config/nvim/snippets/tex.lua
+++ neovim-lua/.config/nvim/snippets/tex.lua
@@ -1,5 +1,11 @@
+local conds = require("luasnip.extras.expand_conditions")
+
+local beg = parse({trig = "beg", wordTrig=true}, "\\begin{$1}\n\t$2\n\\end{$1}")
+local bal = parse({trig = "bal", wordTrig=true }, "\\begin{align}\n\t$1\n\\end{align}")
+beg.condition = conds.line_begin
+bal.condition = conds.line_begin
+
 return {
 },{
-	parse({trig = "beg", wordTrig=true }, "\\begin{$1}\n\t$2\n\\end{$1}"),
-	parse({trig = "bal", wordTrig=true }, "\\begin{align}\n\t$1\n\\end{align}"),
+	beg, bal
 }
