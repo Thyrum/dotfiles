658570bd94e487c41f021ed313b919d5d1c275ac
Thyrum <adj00@protonmail.com>
2022-11-07 16:15:39 +0100

    [neovim-lua] Fix latex snippets autoformat combination
    The autoformat would interfere with lingering multicursors from the tex
    snippets. Whenever `$0` is reached in a latex snippet, the snippet is
    removed from the jumplist and disbanded. This now occurs earlier in the
    snippet.

--- neovim-lua/.config/nvim/snippets/tex.lua
+++ neovim-lua/.config/nvim/snippets/tex.lua
@@ -1,7 +1,7 @@
 local conds = require("luasnip.extras.expand_conditions")
 
-local beg = parse({trig = "beg", wordTrig=true}, "\\begin{$1}\n\t$2\n\\end{$1}")
-local bal = parse({trig = "bal", wordTrig=true }, "\\begin{align}\n\t$1\n\\end{align}")
+local beg = parse({trig = "beg", wordTrig=true}, "\\begin{$1}\n\t$0\n\\end{$1}")
+local bal = parse({trig = "bal", wordTrig=true }, "\\begin{align}\n\t$0\n\\end{align}")
 beg.condition = conds.line_begin
 bal.condition = conds.line_begin
 
