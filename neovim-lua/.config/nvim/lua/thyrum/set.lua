feb322326523303e360d8e9997d6f05a8de12ebd
Thyrum <adj00@protonmail.com>
2022-12-30 12:04:32 +0100

    [neovim-lua] Reorganise `set.lua'

--- neovim-lua/.config/nvim/lua/thyrum/set.lua
+++ neovim-lua/.config/nvim/lua/thyrum/set.lua
@@ -1,40 +1,48 @@
+-- visual
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.showbreak= "↪ "
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+
 vim.opt.listchars = "tab:┆ "
 vim.opt.list = true
+
+vim.opt.showbreak= "↪ "
+vim.opt.breakindent = true
+vim.opt.wrap = true
+
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "81,101"
+
+vim.opt.shortmess:append("c")
+
+-- interactive
+vim.opt.mouse = "a"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.scrolloff = 3
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.errorbells = false
+
+vim.g.mapleader = " "
 
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.errorbells = false
+
+-- indentation
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
-
 vim.opt.smartindent = true
 
-vim.opt.wrap = true
-
+-- files
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 3
 vim.opt.isfname:append("@-@")
-
-vim.opt.shortmess:append("c")
-
-vim.opt.colorcolumn = "81,101"
-
-vim.g.mapleader = " "
